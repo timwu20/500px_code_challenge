298d22e6969ea1d4ff6e4b6958128e9f87873903
Tim <tim@timwu.ca>
2016-05-12 11:47:04 -0400

    logout functionality, semantic-ui messages, like and unlike functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 # for sessions
 app.secret_key = b'\xdf\xf6\xfd\xb8\xdcI\xa85n\x10\x03\x88\x12\\\xc1\xb4\x80\x8f\x18@\x83{\xe1\xa5'
 
-from flask import session, redirect, url_for, request, flash, render_template
+from flask import session, redirect, url_for, request, flash, render_template, jsonify
 from flask_oauthlib.client import OAuth
 import requests
 from werkzeug.contrib.cache import SimpleCache
@@ -16,7 +16,7 @@ CONSUMER_SECRET = 'SWVn3TDH6d0GjBRoe1eySab3H52JJ4ipDGjCVnGi'
 
 base_url = 'https://api.500px.com/v1/'
 
-'''
+
 oauth = OAuth()
 
 _500px = oauth.remote_app('500px',
@@ -31,7 +31,6 @@ _500px = oauth.remote_app('500px',
 @_500px.tokengetter
 def get_500px_token(token=None):
     return session.get('500px_token')
-'''
 
 @app.route('/')
 def index():
@@ -59,21 +58,54 @@ def index():
                 raise Exception('No photos found in data')
 
         except Exception as e:
-            flash ('Something went wrong getting photos from 500px: %s' % e)
+            flash ('Something went wrong getting photos from 500px: %s' % e, 'negative')
             photos = []
     else:
         app.logger.debug('photos fetched from cache')
 
     context = {
             'photos': photos,
+            'authenticated': False if not get_500px_token() else get_500px_token(),
         }
     return render_template('index.html', **context)
 
-'''
+@app.route('/like/<photo_id>', methods=['POST'])
+def like(photo_id):
+    r = _500px.post('photos/%s/vote' % photo_id, data={
+        'vote': 1,
+    })
+
+    json = r.data
+
+    #purge index cache
+    cache.delete('photos')
+
+    return jsonify( **json )
+
+@app.route('/delete-like/<photo_id>', methods=['POST'])
+def delete_like(photo_id):
+    r = _500px.delete('photos/%s/vote' % photo_id)
+
+    json = r.data
+
+    #purge index cache
+    cache.delete('photos')
+    
+    return jsonify( **json )
+
+
 @app.route('/login')
 def login():
     return _500px.authorize(callback=url_for('oauth_authorized',
         next=request.args.get('next') or request.referrer or None))
+
+@app.route('/logout')
+def logout():
+    next_url = request.args.get('next') or url_for('index')
+    session.pop('500px_token', None)
+    flash('You were successfully logged out', 'positive')
+    return redirect(next_url)
+
 
 @app.route('/oauth-authorized')
 def oauth_authorized():
@@ -89,9 +121,8 @@ def oauth_authorized():
         resp['oauth_token_secret']
     )
 
-    flash('You were successfully signed in')
+    flash('You were successfully signed in', 'positive')
     return redirect(next_url)
-'''
 
 if __name__ == '__main__':
     app.run(debug=True)
